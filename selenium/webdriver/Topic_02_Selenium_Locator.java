da168197791951996af51a99afdd4fa84aa6c692
nam <namhoang.law@gmail.com>
2023-09-15 23:24:13 +0700

    Create new repository

--- selenium/webdriver/Topic_02_Selenium_Locator.java
+++ selenium/webdriver/Topic_02_Selenium_Locator.java
@@ -22,15 +22,46 @@ public class Topic_02_Selenium_Locator {
 
         driver = new FirefoxDriver();
         driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-        driver.manage().window().maximize();
+        driver.get("https://demo.nopcommerce.com/register");
+    }
+    //TestNG: Order testcase theo Alphabet
+    // Firstname textbox - HTML code
+    // HTML
+    /*
+    *<input type="text" data-val="true" data-val-required="First name is required." id="FirstName" name="FirstName">
+     */
+
+    @Test
+    public void TC_01_ID() {
+//        driver.findElement(By.)
+    }
+    @Test
+    public void TC_02_Class() {
+
     }
 
     @Test
-    public void TC_01_() {
+    public void TC_03_Name() {
 
     }
+    @Test
+    public void TC_04_Tagname() {
 
-    public void TC_02_() {
+    }
+    @Test
+    public void TC_05_LinkText() {
+
+    }
+    @Test
+    public void TC_06_Partial_Link() {
+
+    }
+    @Test
+    public void TC_07_CSS() {
+
+    }
+    @Test
+    public void TC_08_Xpath() {
 
     }
 }
