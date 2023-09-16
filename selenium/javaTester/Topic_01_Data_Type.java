4f93d882c87d9fd958cb524593d796563b69f1aa
nam <namhoang.law@gmail.com>
2023-09-16 10:49:10 +0700

    Create new repository

---
+++ selenium/javaTester/Topic_01_Data_Type.java
@@ -0,0 +1,40 @@
+package javaTester;
+
+import java.lang.reflect.Array;
+
+public class Topic_01_Data_Type {
+    // Kiểu dữ liệu trong Java - 2 nhóm
+
+    // I - Kiểu dữ liệu nguyên thủy (Primitive type)
+    // 8 loại
+    // Số nguyên: byte - short - int - long
+    // Ko có phần thập phân: nhân viên trong 1 công ty/ học sinh trong 1 lớp/ trường/..
+
+    // Số thực: float - double
+    // Có phần thập phân: điểm số/ lương/..
+
+    // Kí tự: char
+    // Logic: boolean
+
+    // II - Kiểu dữ liệu tham chiếu (Reference Type)
+    // CLass
+
+    //Object
+
+    //Array
+    int[] studentName = {15, 20};
+
+    String[] studentNames = {"Automation", "Testing"};
+
+    //Collection: List/ Set/ Queue
+
+    //String
+
+    // Khai báo 1 biến để lưu trữ 1 loại dữ liệu
+    // Access modifer: phạm vi truy cập (public/ private/ protected/ default)
+    // Kiểu dữ liệu
+    // Tên biến
+    // Giá trị của biến - gán vào phép =
+    // Nếu như không gán: dữ liệu mặc định
+    int studentNumber = 200;
+}
--- selenium/webdriver/Topic_02_Selenium_Locator.java
+++ selenium/webdriver/Topic_02_Selenium_Locator.java
@@ -1,5 +1,6 @@
 package webdriver;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.BeforeClass;
@@ -33,35 +34,69 @@ public class Topic_02_Selenium_Locator {
 
     @Test
     public void TC_01_ID() {
-//        driver.findElement(By.)
+        // Tìm element có id là FirstName
+        driver.findElement(By.id("FirstName")).sendKeys("Namprovip");
+        System.out.println(driver.findElement(By.id("FirstName")));
     }
     @Test
     public void TC_02_Class() {
-
+        driver.findElement(By.className("header-logo"));
     }
 
     @Test
     public void TC_03_Name() {
-
+        driver.findElement(By.name("DateOfBirthDay"));
     }
     @Test
     public void TC_04_Tagname() {
-
+        driver.findElements(By.tagName("input"));
     }
     @Test
     public void TC_05_LinkText() {
-
+        // Độ chính xác cao = tuyệt đối = lấy toàn bộ
+        driver.findElement(By.linkText("Shipping & returns"));
     }
     @Test
     public void TC_06_Partial_Link() {
-
+        // Độ chính xa ko cao = tương đối = lấy 1 phần
+        driver.findElement(By.partialLinkText("vendor account"));
     }
     @Test
     public void TC_07_CSS() {
+    // Css vs ID
+        driver.findElement(By.cssSelector("input#FirstName"));
+        driver.findElement(By.cssSelector("input[id=FirstName]"));
+        driver.findElement(By.cssSelector("#FirstName"));
+    // Css vs Class
+        driver.findElement(By.cssSelector("div[class='page-title"));
+        driver.findElement(By.cssSelector("div.page-title"));
+        driver.findElement(By.cssSelector(".page-title"));
+     // Css vs Name
+        driver.findElement(By.cssSelector("input[name='FirstName']"));
+     // Css vs tagname
+        driver.findElement(By.cssSelector("input"));
+     // Css vs Partial link
+        driver.findElement(By.cssSelector("a[href*='addresses']"));
+//        driver.findElement(By.cssSelector("a[href^='addresses']"));
+//        driver.findElement(By.cssSelector("a[href$='addresses']"));
 
     }
     @Test
     public void TC_08_Xpath() {
+        // Xpath vs ID
+        driver.findElement(By.xpath("//input[@id='FirstName']"));
+        // Xpath vs Class
+        driver.findElement(By.xpath("//div[@class='page-title']"));
+        // Css vs Name
+        driver.findElement(By.xpath("//input[@name='FirstName']"));
+        // Xpath vs TagName
+        driver.findElement(By.xpath("//input"));
+        // Xpath vs Link
+        driver.findElement(By.xpath("//a[@href='/customer/addresses']"));
+        driver.findElement(By.xpath("//a[text()='Addresses']"));
 
+        // Xpath vs Partial link
+        driver.findElement(By.xpath("//a[contains(@href,'addresses')]"));
+        driver.findElement(By.xpath("//a[contains(text(),'Addresses')]"));
     }
 }
