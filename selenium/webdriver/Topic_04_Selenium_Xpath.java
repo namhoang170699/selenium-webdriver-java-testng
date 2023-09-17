48a51c5e66a4456b38ededbb37c6aa3d64a18994
nam <namhoang.law@gmail.com>
2023-09-17 14:51:39 +0700

    8 locator exercise

---
+++ selenium/webdriver/Topic_04_Selenium_Xpath.java
@@ -0,0 +1,42 @@
+package webdriver;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+public class Topic_04_Selenium_Xpath {
+    WebDriver driver;
+    String projectPath = System.getProperty("user.dir");
+    String osName = System.getProperty("os.name");
+
+    @BeforeClass
+    public void beforeClass() {
+        if (osName.contains("Windows")) {
+            System.setProperty("webdriver.gecko.driver", projectPath + "\\browserDrivers\\geckodriver.exe");
+        } else {
+            System.setProperty("webdriver.gecko.driver", projectPath + "/browserDrivers/geckodriver");
+        }
+
+        driver = new FirefoxDriver();
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        driver.get("https://demo.nopcommerce.com/electronics");
+    }
+
+    @Test
+    public void TC_01_() {
+
+    }
+
+    public void TC_02_() {
+
+    }
+    @AfterClass
+    public void afterClass() {
+
+        driver.quit();
+    }
+}
