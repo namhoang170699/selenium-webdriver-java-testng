f4ba2ea0e612a70363a95a0b77d617850d345b76
nam <namhoang.law@gmail.com>
2023-10-23 23:14:07 +0700

    handle button radio check box

--- selenium/webdriver/Topic_17_Button_Radio_Checkbox.java
+++ selenium/webdriver/Topic_17_Button_Radio_Checkbox.java
@@ -1,8 +1,6 @@
 package webdriver;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.Color;
 import org.testng.Assert;
@@ -54,28 +52,42 @@ public class Topic_17_Button_Radio_Checkbox {
 
     }
     @Test
-    public void TC_02_Telerik_checkbox() {
+    public void TC_02_Telerik_checkbox_radio() {
         driver.get("https://demos.telerik.com/kendo-ui/checkbox/index");
-        WebElement dualZoneCheckbox = driver.findElement(By.cssSelector("input#eq5"));
-        dualZoneCheckbox.click();
-        Assert.assertTrue(dualZoneCheckbox.isSelected());
-        dualZoneCheckbox.click();
-        Assert.assertFalse(dualZoneCheckbox.isSelected());
+        elementClick(By.cssSelector("input#eq5"));
+        isElementSelected(By.cssSelector("input#eq5"));
 
         driver.get("https://demos.telerik.com/kendo-ui/radiobutton/index");
+        elementClick(By.cssSelector("input#engine3"));
+        isElementSelected(By.cssSelector("input#engine3"));
+    }
+    @Test
+    public void TC_03_Angular_checkbox_radio() {
+        driver.get("https://material.angular.io/components/radio/examples");
+        elementClick(By.cssSelector("input#mat-radio-4-input"));
+        Assert.assertTrue(isElementSelected(By.cssSelector("input#mat-radio-4-input")));
 
-        WebElement radioButton = driver.findElement(By.cssSelector("input#engine3"));
-        radioButton.click();
-        radioButton.isSelected();
-        if (radioButton.isSelected()) {
-            System.out.println("Radio button is selected");
-        } else{
-            radioButton.click();
-        };
+        driver.get("https://material.angular.io/components/checkbox/examples");
+        elementClick(By.cssSelector("input#mat-mdc-checkbox-1-input"));
+        elementClick(By.cssSelector("input#mat-mdc-checkbox-2-input"));
+        Assert.assertTrue(isElementSelected(By.cssSelector("input#mat-mdc-checkbox-1-input")));
+        Assert.assertTrue(isElementSelected(By.cssSelector("input#mat-mdc-checkbox-2-input")));
+
+        elementClick(By.cssSelector("input#mat-mdc-checkbox-1-input"));
+        elementClick(By.cssSelector("input#mat-mdc-checkbox-2-input"));
+        Assert.assertFalse(isElementSelected(By.cssSelector("input#mat-mdc-checkbox-1-input")));
+        Assert.assertFalse(isElementSelected(By.cssSelector("input#mat-mdc-checkbox-2-input")));
 
 
     }
-    @AfterClass
+    @Test
+    public void TC_04_Tiemchungcovid19_custom_checkbox_radio() {
+        driver.get("https://tiemchungcovid19.gov.vn/portal/register-person");
+        WebElement radioButton = driver.findElement(By.cssSelector("input#mat-radio-3-input"));
+        ((JavascriptExecutor) driver).executeScript("arguments[0].click();",radioButton);
+        sleepInSeconds(5);
+    }
+        @AfterClass
     public void afterClass() {
 
         driver.quit();
@@ -87,5 +99,20 @@ public class Topic_17_Button_Radio_Checkbox {
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
+    }
+    public boolean isElementSelected(By by) {
+        WebElement element = driver.findElement(by);
+        if (element.isSelected()) {
+            System.out.println("Element is selected:" + by);
+            return true;
+        }else {
+            System.out.println("Element is not selected:" + by);
+            return false;
+        }
+    }
+
+    public void elementClick(By by) {
+        WebElement element = driver.findElement(by);
+        element.click();
     }
 }
